8edfe66b7547cdb4eed3431c9c0bb7e7f5a13a72
Julian2455 <juliangaldamez0@gmail.com>
2024-09-21 23:11:13 -0300

    diseño ok, agregar redirecciones cuando termine otros componentes

--- src/components/login.js
+++ src/components/login.js
@@ -1,14 +1,13 @@
 import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
+import '../styles/login.css';
+import '../styles/loading.css'
 
 function Login() {
   const [correo, setCorreo] = useState('');
   const [password, setPassword] = useState('');
   const [userData, setUserData] = useState(null); 
-  const [validated, setValidated] = useState(false);
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
   const handleCorreoChange = (event) => {
     setCorreo(event.target.value);
   };
@@ -17,9 +16,11 @@ function Login() {
   };
   const handleSubmit = (event) => {
     event.preventDefault();
+    setLoading(true);
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.stopPropagation();
+      setLoading(false)
     }else{
       const data = {
         email_usuario: correo, 
@@ -33,38 +34,65 @@ function Login() {
         body: JSON.stringify(data)
       })
       .then(response => {
+        setLoading(false);
         if (!response.ok) {
-          throw new Error('Error en la autenticación');
+          if (response.status === 401) {
+            throw new Error('Correo o contraseña incorrectos');
+          }
+          throw new Error('Error en la solicitud');
         }
         return response.json();
       })
       .then(data => {
         setUserData(data);
+        setError(''); 
       })
+      .catch(error => {
+        setError(error.message);
+        setLoading(false)
+        console.error(error); 
+      });     
     }
-    setValidated(true);
   };
   return (
-    <div>
-      <Form noValidate validated={validated} onSubmit={handleSubmit}>
-        <Row className="mb-3">
-          <Form.Group as={Col} md="6" controlId="validationCustom01">
-            <Form.Label>Correo</Form.Label>
-            <Form.Control type="email" value={correo} onChange={handleCorreoChange} required/>
-          </Form.Group>
-        </Row>
-        <Row className="mb-3">
-          <Form.Group as={Col} md="6" controlId="validationCustom03">
-            <Form.Label>Contraseña</Form.Label>
-            <Form.Control type="password" value={password} onChange={handlePasswordChange} required />
-          </Form.Group>
-        </Row>
-        <Button type="submit">Iniciar sesion</Button>
-      </Form>
-        {userData && (
+    <div className="d-flex justify-content-center align-items-center min-vh-100">
+      <div className='containter'>
+      <div className="form-container">
+        <p className="title">Iniciar Sesión</p>
+        {error && <p className="error-message">Error: {error}</p>}
+        <form className="form" onSubmit={handleSubmit}>
+          <input type="email" className="input" placeholder="Email" value={correo} onChange={handleCorreoChange} required disabled={loading}/>
+          <input type="password" className="input" placeholder="Contraseña" value={password} onChange={handlePasswordChange} required disabled={loading}/>
+          <p className="page-link">
+            <span className="page-link-label">Olvido su contraseña?</span>
+          </p>
+          <button className="form-btn" disabled={loading}>{loading ? 'Cargando...' : 'Iniciar Sesión'}</button>
+        </form>
+        <p className="sign-up-label">
+          No tienes cuenta?<span className="sign-up-link">Registrarme</span>
+        </p>
+      </div>
+      </div>
+      {loading && (
+        <div className="spinner center">
+          <div className="spinner-blade" />
+          <div className="spinner-blade" />
+          <div className="spinner-blade" />
+          <div className="spinner-blade" />
+          <div className="spinner-blade" />
+          <div className="spinner-blade" />
+          <div className="spinner-blade" />
+          <div className="spinner-blade" />
+          <div className="spinner-blade" />
+          <div className="spinner-blade" />
+          <div className="spinner-blade" />
+          <div className="spinner-blade" />
+        </div>
+      )}{/*Reemplazar por la redireccion cuando termine los otros componentes*/}
+      {userData && (
           <div className="container mt-4">
             <h5>Datos del usuario:</h5>
-            <pre>{JSON.stringify(userData, null, 2)}</pre>
+            <pre>{JSON.stringify(userData, null, 2)}</pre> 
           </div>
         )}
     </div>
